f83db64007492f0a76b2aa1a99258f11df54a887
Muhammed Ikharo <muhammedikharo@gmail.com>
2018-01-20 23:16:53 -0800

    v1 complete
    Able to find and print item prices through item ID found in url.

---
+++ amazonapi.js
@@ -0,0 +1,44 @@
+/* v1: Find price by product IDs from Amazon URL <-- Complete with sample testID
+   v2: Find price by generic item name (cup, brush, toothbrush)
+
+  ################	
+  # Dependencies #          
+  ################
+   npm install jsdom <- xml element printing by ID
+   npm install apac  <- nodejs wrapper of Amazon Product Advertising API
+*/
+
+const jsdom = require("jsdom");
+
+const { JSDOM } = jsdom;
+
+const {OperationHelper} = require('apac');
+
+const opHelper = new OperationHelper({
+    awsId:     'AKIAISF2B2SBCCCDF7LA',
+    awsSecret: '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',
+    assocId:   'rainyday0d-20' 
+});
+
+opHelper.execute('ItemLookup', {
+  //'SearchIndex': 'Books',
+  //'Keywords': 'Andis 17150 Profoil Lithium',
+  'ItemId': 'B000LNB8HK',
+  'ResponseGroup': 'ItemAttributes'
+}).then((response) => {
+    //console.log("Results object: ", response.result);
+
+    //console.log("Raw response body: ", response.responseBody) <--- Use to view total structure 
+
+	var xml = response.responseBody;
+
+	const dom = new JSDOM(xml);
+	
+	console.log(dom.window.document.querySelector("FormattedPrice").textContent);
+	}
+
+).catch((err) => {
+    console.error("Something went wrong! ", err);
+	});
+
+
