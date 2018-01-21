e79314825bf1330428c5f62a70efb79a1f1fd1cd
lovick <lovickk@gmail.com>
2018-01-20 19:53:28 -0800

    Basic site up.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import webapp2
+import os
+from google.appengine.ext.webapp import template
 
 class RainyDay(webapp2.RequestHandler):
     def get(self):
-        self.response.write('This is a <b>test</b>')
+        values = 'test vasdlue'
+        self.response.out.write(template.render('index.html', values))
 
 app = webapp2.WSGIApplication([
     ('/', RainyDay),
